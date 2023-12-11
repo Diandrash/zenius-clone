4367f6a914d4b71873ab15d7a038499e290bdd9f
diandrash <fshadeva@gmail.com>
2023-12-11 22:17:03 +0700

    Responsive Mobile

---
+++ script.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector('.nav-list');
+const menu = document.querySelector('.icon-menu');
+
+menu.addEventListener('click', function () {
+    nav.classList.toggle('translate-x-0')    
+})
