f7098f7dedcc399ebdbfb74fac63efceeee43fd3
diandrash <fshadeva@gmail.com>
2023-12-12 21:44:58 +0700

    Finish Responsive Tablet

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const nav = document.querySelector('.nav-list');
+const nav = document.querySelector('.nav-lists');
+const navList = document.querySelectorAll(".nav-list");
 const menu = document.querySelector('.icon-menu');
 
 menu.addEventListener('click', function () {
+    // alert('halo');
     nav.classList.toggle('translate-x-0')    
 })
+
+// navList.addEventListener('click', function () {
+//     alert('halo');
+//     nav.classList.toggle('translate-x-full')   
+     
+// })
+
+// for (let i = 0; i < navList.length; i++) {
+//     navList[i].addEventListener('click', function () {
+//         // alert('tes');
+//         nav.classList.toggle('translate-x-full')   
+//     })
+// }
